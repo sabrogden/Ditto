1b65fbf74efb6a779a6ce0dfc60b7c6f86fa654d
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2008-01-30 03:28:23 +0000

    added new themes [SAB]
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@432 595ec19a-5cb4-439b-94a8-42fb3063c22c

--- WebSite/themes/index.php
+++ WebSite/themes/index.php
@@ -48,22 +48,33 @@
       <li>Open Ditto - Options - Quick Paste and select the new them in the 
       theme combo</li>
     </ol>
-    <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%" id="AutoNumber5">
+    <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%" id="AutoNumber5" height="182">
       <tr>
-        <td width="8%"><a href="Nighted.png">
+        <td width="9%" height="84" align="center"><a href="Nighted.png">
         <img border="0" src="Nighted_small.PNG" width="72" height="84"></a></td>
-        <td width="13%"><a href="Nighted.xml">Nighted</a></td>
-        <td width="69%">Suggested font &quot;<a href="BitDust%20Two.ttf">BitDust Two</a>&quot; 
+        <td width="12%" height="84" align="center"><a href="Nighted.xml">Nighted</a></td>
+        <td width="69%" height="84">Suggested font &quot;<a href="BitDust%20Two.ttf">BitDust Two</a>&quot; 
         size 6</td>
       </tr>
       <tr>
-        <td width="8%">&nbsp;<p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</td>
-        <td width="13%">&nbsp;</td>
-        <td width="69%">&nbsp;</td>
+        <td width="9%" height="79" align="center"><a href="DittoBOCA.jpg">
+        <img border="0" src="DittoBOCA.jpg" width="72" height="63"></a></td>
+        <td width="12%" height="79" align="center"><a href="BOCApastel.xml">Sweden Flag </a></td>
+        <td width="69%" height="79">This theme could be considered a Sweden Flag 
+        theme with light colours.</td>
+      </tr>
+      <tr>
+        <td width="9%" height="79" align="center"><a href="DittoTheme%20Blau%20Screenshot.jpg">
+        <img border="0" src="DittoTheme%20Blau%20Screenshot_small.JPG" width="72" height="60"></a></td>
+        <td width="12%" height="79" align="center"><a href="OnnoG%20Blue.xml">Blue Theme</a></td>
+        <td width="69%" height="79">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="9%" height="79" align="center">
+        <a href="DittoTheme%20Gruen%20Screenshot.jpg">
+        <img border="0" src="DittoTheme%20Gruen%20Screenshot_small.jpg" width="72" height="62"></a></td>
+        <td width="12%" height="79" align="center"><a href="OnnoG%20Green.xml">Green Theme</a></td>
+        <td width="69%" height="79">&nbsp;</td>
       </tr>
     </table>
 
