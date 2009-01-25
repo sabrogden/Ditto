47bb64fb120d753a4efbebcc25c0cdd4c017f311
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2009-01-25 19:39:42 +0000

    re added changes made by keven that i overwrote.  Attache to the window thread before setting focus
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@446 595ec19a-5cb4-439b-94a8-42fb3063c22c

--- CP_Main.cpp
+++ CP_Main.cpp
@@ -420,8 +420,28 @@ bool CCP_MainApp::TargetActiveWindow()
 
 bool CCP_MainApp::ActivateTarget()
 {
-	::SetForegroundWindow(m_hTargetWnd);
+	DWORD tidTarget = ::GetWindowThreadProcessId(m_hTargetWnd, NULL);
+	DWORD tidSelf = ::GetCurrentThreadId();
+
+	// We can't set the focus window from another thread,
+	// so we have to attach to the thread input first:
+	::AttachThreadInput(tidTarget, tidSelf, TRUE);
+
 	::SetFocus(m_hTargetWnd);
+
+	//	Log( StrF(
+	//		_T("ActivateTarget - AFTER:") \
+	//		_T("\n\tm_hTargetWnd = %s") \
+	//		_T("\n\tGetForegroundWindow = %s") \
+	//		_T("\n\tGetActiveWindow = %s") \
+	//		_T("\n\tGetFocus = %s\n"),
+	//			GetParentsString(m_hTargetWnd),
+	//			GetParentsString(::GetForegroundWindow()),
+	//			GetParentsString(::GetActiveWindow()),
+	//			GetParentsString(::GetFocus()) ) );
+
+	// Detach from thread input
+	::AttachThreadInput(tidTarget, tidSelf, FALSE);
 
 	return true;
 }
