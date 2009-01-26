887a74eb5f5c09c628bf36ec8cd284544cc5a772
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2009-01-26 05:42:52 +0000

    removed build error on some vista machines with richedit import
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@449 595ec19a-5cb4-439b-94a8-42fb3063c22c

--- StdAfx.h
+++ StdAfx.h
@@ -36,7 +36,7 @@
 #include "UnicodeMacros.h"
 
 #include <imm.h>
-#import "riched20.dll" raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#import "riched20.dll" raw_interfaces_only, raw_native_types, no_namespace, named_guids, exclude("UINT_PTR") 
 
 
 //{{AFX_INSERT_LOCATION}}
