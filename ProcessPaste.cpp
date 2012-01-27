5274940d2cc19e81e8de4b1bd59c74276b8ce86f
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2012-01-27 02:26:31 +0000

    fixed issue that prevented last pasted time from getting saved to the db
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@628 595ec19a-5cb4-439b-94a8-42fb3063c22c

--- ProcessPaste.cpp
+++ ProcessPaste.cpp
@@ -167,7 +167,7 @@ UINT CProcessPaste::MarkAsPastedThread(LPVOID pParam)
 
 			try
 			{
-				theApp.m_db.execDMLEx(_T("UPDATE Main SET lastPasteDate = %d where lID = %d;"), CTime::GetCurrentTime().GetTime(), pData);
+				theApp.m_db.execDMLEx(_T("UPDATE Main SET lastPasteDate = %d where lID = %d;"), CTime::GetCurrentTime().GetTime(), pData->clipId);
 			}
 			CATCH_SQLITE_EXCEPTION
 
