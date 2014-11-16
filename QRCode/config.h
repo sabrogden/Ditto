f2f33a35366c75e8c932ef4fa4b8e223cd26d77a
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2014-11-16 04:39:21 +0000

    First pass at qr code generation
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@720 595ec19a-5cb4-439b-94a8-42fb3063c22c

---
+++ QRCode/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// manual config.h for microsoft visual studio
+
+#define HAVE_STRDUP 1
+
+#define __STATIC static
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define MICRO_VERSION 0
+#define VERSION ""
+
+#endif
