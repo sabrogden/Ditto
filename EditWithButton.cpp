d9891d799c3cc2436a9119a3cce0b4744d0ed8cc
sabrogden <sabrogden@595ec19a-5cb4-439b-94a8-42fb3063c22c>
2014-12-31 20:30:17 +0000

    ctrl-c/v/z/x/a work now in the search edit box
    git-svn-id: svn://svn.code.sf.net/p/ditto-cp/code/trunk@739 595ec19a-5cb4-439b-94a8-42fb3063c22c

--- EditWithButton.cpp
+++ EditWithButton.cpp
@@ -55,6 +55,29 @@ void CEditWithButton::PreSubclassWindow( )
 
 BOOL CEditWithButton::PreTranslateMessage( MSG* pMsg )
 {
+	// TODO: Add your specialized code here and/or call the base class
+	// Intercept Ctrl + Z (Undo), Ctrl + X (Cut), Ctrl + C (Copy), Ctrl + V (Paste) and Ctrl + A (Select All)
+	// before CEdit base class gets a hold of them.
+	if (pMsg->message == WM_KEYDOWN && ::GetKeyState(VK_CONTROL) < 0)
+		switch (pMsg->wParam)
+	{
+		case 'Z':
+			Undo();
+			return TRUE;
+		case 'X':
+			Cut();
+			return TRUE;
+		case 'C':
+			Copy();
+			return TRUE;
+		case 'V':
+			Paste();
+			return TRUE;
+		case 'A':
+			SetSel(0, -1);
+			return TRUE;
+	}
+
 	switch(pMsg->message) 
 	{
 	case WM_KEYDOWN:
